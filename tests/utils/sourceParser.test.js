b1f82e5332dca3efd1b52291c1e8e578a81d934e
Tyler Green <tyler.green@intersection.com>
2017-09-13 09:08:54 -0400

    Setup test framework for sourceParser

---
+++ tests/utils/sourceParser.test.js
@@ -0,0 +1,8 @@
+const readNote = require('../../lib/utils/readNote');
+const sourceParser = require('../../lib/utils/sourceParser');
+
+describe('The source parser', () => {
+  it('should return true', () => {
+    expect(true).toBe(true);
+  });
+});
