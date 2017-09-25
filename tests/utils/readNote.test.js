3ab968d1370f90c5558825af59f57910597ddb32
Tyler Green <tyler.green@intersection.com>
2017-09-25 08:51:51 -0400

    Update createdAt for Notes to use Date object

--- tests/utils/readNote.test.js
+++ tests/utils/readNote.test.js
@@ -3,8 +3,8 @@ const Note = require('../../lib/model/Note');
 
 describe('The note reader', () => {
   it('should read a simple note', async () => {
-    await expect(readNote('./tests/fixtures/simple_note.md')).resolves.toHaveProperty('title');
-    await expect(readNote('./tests/fixtures/simple_note.md')).resolves.toHaveProperty('tags');
-    await expect(readNote('./tests/fixtures/simple_note.md')).resolves.toHaveProperty('body');
+    await expect(readNote('./tests/fixtures/2017-09-25-08-46-00.md')).resolves.toHaveProperty('title');
+    await expect(readNote('./tests/fixtures/2017-09-25-08-46-00.md')).resolves.toHaveProperty('tags');
+    await expect(readNote('./tests/fixtures/2017-09-25-08-46-00.md')).resolves.toHaveProperty('body');
   });
 });
