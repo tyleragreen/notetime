38267e0121c9e57a518f294cfb2173b836d15202
Tyler Green <tyler.green@intersection.com>
2018-01-06 12:02:32 -0500

    Change to require identifier on complex sources

--- tests/utils/sourceParser.test.js
+++ tests/utils/sourceParser.test.js
@@ -2,6 +2,10 @@ const readNote = require('../../lib/utils/readNote');
 const sourceParser = require('../../lib/utils/sourceParser');
 const lineParser = require('../../lib/utils/lineParser');
 
+const invalidSource = [
+  '; source-bad This tag is not recognized',
+];
+
 const invalidSingleSource = [
   "; source-id nyt",
   "; source-publication New York Times",
@@ -30,15 +34,12 @@ const validTripleSource = [
   "; 1-source-title Title 1",
   "; 4-source-title Title 3",
   "; 2-source-title Title 2",
+  "; 2-source-author Author 2",
 ];
 
 const multipleFirstSources = [
   '; source-title Title 1',
   '; 1-source-title Title 2',
-];
-
-const invalidSource = [
-  '; source-bad This tag is not recognized',
 ];
 
 describe('The source parser', () => {
@@ -110,6 +111,7 @@ describe('The source parser', () => {
     expect(sources.length).toBe(3);
     expect(sources[0].title).toBe('Title 1');
     expect(sources[1].title).toBe('Title 2');
+    expect(sources[1].author).toBe('Author 2');
     expect(sources[2].title).toBe('Title 3');
   });
 
@@ -117,55 +119,23 @@ describe('The source parser', () => {
     expect(() => {
       sourceParser(invalidSingleSource)
     }).toThrow(Error);
-    //const sources = sourceParser(singleSource);
-    //const source = sources[0];
-
-    //expect(sources.length).toBe(1);
-    //expect(source.publication).toBe(lineParser(singleSource[1]));
-    //expect(source.title).toBe(lineParser(singleSource[2]));
-    //expect(source.date.print()).toBe(lineParser(singleSource[3]));
-    //expect(source.url).toBe(lineParser(singleSource[4]));
   });
 
   it('should fail to parse multiple complex sources if one is missing an identifier', () => {
     expect(() => {
       sourceParser(invalidMultiSource)
     }).toThrow(Error);
-    //const sources = sourceParser(multiSource);
-    //const firstSource = sources[0];
-    //const secondSource = sources[1];
-
-    //expect(sources.length).toBe(2);
-    //expect(firstSource.publication).toBe(lineParser(multiSource[1]));
-    //expect(firstSource.title).toBe(lineParser(multiSource[2]));
-    //expect(firstSource.date.print()).toBe(lineParser(multiSource[3]));
-    //expect(firstSource.url).toBe(lineParser(multiSource[4]));
-
-    //expect(secondSource.publication).toBe(lineParser(multiSource[6]));
-    //expect(secondSource.title).toBe(lineParser(multiSource[7]));
-    //expect(secondSource.date.print()).toBe(lineParser(multiSource[8]));
-    //expect(secondSource.url).toBe(lineParser(multiSource[9]));
   });
 
   it('should fail to parse three complex sources if one is missing an identifier', () => {
     expect(() => {
       sourceParser(invalidTripleSource)
     }).toThrow(Error);
-    //const sources = sourceParser(tripleSource);
-
-    //expect(sources[0].title).toBe(lineParser(tripleSource[0]));
-    //expect(sources[1].title).toBe(lineParser(tripleSource[2]));
-    //expect(sources[2].title).toBe(lineParser(tripleSource[1]));
   });
 
   it('should parse two sources with the different first notation', () => {
     expect(() => {
       sourceParser(multipleFirstSources)
     }).toThrow(Error);
-    //const sources = sourceParser(multipleFirstSources);
-
-    //expect(sources.length).toBe(2);
-    //expect(sources[0].title).toBe(lineParser(multipleFirstSources[0]));
-    //expect(sources[1].title).toBe(lineParser(multipleFirstSources[1]));
   });
 });
